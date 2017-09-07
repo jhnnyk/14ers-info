eefb0b1877a0321adc495a113942d0f3cfc50bcc
John Knotts <jhnnyk@gmail.com>
2017-09-07 12:58:07 -0600

    access url parameters

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const FOURTEENERS = [
+  {
+    name: 'Grays Peak',
+    elevation: '14270',
+    range: 'Front Range',
+    lat: '39.633820',
+    lon: '-105.817520'
+  },
+  {
+    name: 'Torreys Peak',
+    elevation: '14267',
+    range: 'Front Range',
+    lat: '39.642742',
+    lon: '-105.821259'
+  }
+]
+
+function getURLParameter () {
+  console.log(window.location.search.substring(1))
+}
+
+$(getURLParameter)
